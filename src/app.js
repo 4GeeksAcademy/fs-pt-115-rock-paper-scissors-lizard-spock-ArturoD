8108637af551d6f139ca780927ed50b33462be92
artureey <arturoalexduarte@gmail.com>
2025-06-21 13:41:30 +0000

    TBBT

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
-//write your code here
-console.log("Hello Rigo from the console!");
 
+const options = [`rock`, `paper`, `scissors`, `lizard`, `spock`]
+let userChoice = prompt("ELIGUE TU OPCION: , rock, paper, scissors, lizard, spock").toLowerCase();
+
+const getRandomOption = () => {
+    const index = Math.floor(Math.random() * options.length)
+    return options[index]
+}
+
+const resultUserChoice = (userChoice) => {
+    const computerChoice = getRandomOption();
+    console.log(`el usuario elige ${userChoice}`);
+    console.log(`el sistema elige ${computerChoice}`);
+       
+}
+
+const playGame = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        console.log("Es un empate");
+        
+    }else if ((userChoice === "rock" && (computerChoice === "scissors" || computerChoice === "lizard"))) {
+    console.log("you win");
+    }else if ((userChoice === "paper" && (computerChoice ==="rock" || computerChoice === "spock"))) {
+        console.log("you win");
+        
+    }else if ((userChoice === "scissors" && (computerChoice ==="paper" || computerChoice === "lizard"))) {
+        console.log("you win");
+        
+    }else if ((userChoice === "lizard" && (computerChoice ==="paper" || computerChoice === "spock"))) {
+        console.log("you win");
+        
+    }else if ((userChoice === "spock" && (computerChoice ==="scissors" || computerChoice === "rock"))) {
+        console.log("you win");
+        
+    }
+else {
+    console.log("has perdido intentalo de nuevo");
+    
+}
+
+
+
+}
+playGame(userChoice, getRandomOption());
